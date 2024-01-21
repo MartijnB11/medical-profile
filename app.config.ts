38fec972141797fdff3dc97093febaf8e6dbe9ca
MartijnB11 <martijnbomans11@gmail.com>
2024-01-21 21:02:06 +0100

    easy page + primary colors

---
+++ app.config.ts
@@ -0,0 +1,4 @@
+export default defineAppConfig({
+    primary: 'green',
+    gray: 'cool'
+})
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
-
+    <div class="grid place-content-center h-screen">
+        <h1>Hello World</h1>
+        <UButton>Submit</UButton>
     </div>
 </template>
 
